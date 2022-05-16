0111f3d326ecd928a826533ce9d7145882a3a900
clifstone <47040820+clifstone@users.noreply.github.com>
2022-05-16 16:11:49 -0500

    updating apollo client to new domain

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://www.clif.me/mcwp/graphql",
+    uri: "https://www.kip.cat/mcwp/graphql",
     cache: new InMemoryCache(),
 });
 
